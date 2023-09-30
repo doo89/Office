34f291c85a510ce0a76922197ec6e59048ddc16f
doo89 <lcdoo89@gmail.com>
2023-09-30 10:14:45 +0200

    Create index.ts

---
+++ MonPCFComponentTest/src/index.ts
@@ -0,0 +1,4 @@
+import { IInputs, IOutputs } from 'pcf';
+export class YourPCFComponent implements ComponentFramework.StandardControl<IInputs, IOutputs> {
+  // Implémentez ici votre composant personnalisé
+}
