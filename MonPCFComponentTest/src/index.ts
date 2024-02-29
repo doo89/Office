edba7c7d095640659508475623ef20a478960ac3
doo89 <lcdoo89@gmail.com>
2024-02-29 19:34:30 +0100

    Update index.ts

--- MonPCFComponentTest/src/index.ts
+++ MonPCFComponentTest/src/index.ts
@@ -2,3 +2,4 @@ import { IInputs, IOutputs } from 'pcf';
 export class YourPCFComponent implements ComponentFramework.StandardControl<IInputs, IOutputs> {
   // Implémentez ici votre composant personnalisé
 }
+// A rechercher sur internet
