c93d4e5a4ef2844cbb3c634be51f8915342f2dce
doo89 <lcdoo89@gmail.com>
2024-03-13 06:40:22 +0100

    Delete MonPCFComponentTest directory

--- MonPCFComponentTest/src/index.ts
+++
@@ -1,5 +0,0 @@
-import { IInputs, IOutputs } from 'pcf';
-export class YourPCFComponent implements ComponentFramework.StandardControl<IInputs, IOutputs> {
-  // Implémentez ici votre composant personnalisé
-}
-// A rechercher sur internet
